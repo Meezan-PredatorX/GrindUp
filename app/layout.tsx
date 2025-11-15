730dcc500d1a9e1bee046a7c92e74ce9fe076dc2
Meezan <225868025+Meezan-PredatorX@users.noreply.github.com>
2025-11-15 16:19:51 +0530

    fixed SpeedInsights package import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/lib/context/UserContext";
-import {SpeedInsights} from "@vercel/speed-insights"; 
+import {SpeedInsights} from "@vercel/speed-insights/next"; 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
