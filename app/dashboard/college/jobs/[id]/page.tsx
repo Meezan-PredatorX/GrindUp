72c0558c1d69088dec80ce08702fa369ceeafe20
Meezan-PredatorX <meezan.ansari.px@gmail.com>
2025-11-15 16:10:24 +0530

    Deploy: added author info

---
+++ app/dashboard/college/jobs/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function JobPage () {
+   return (
+      <main className="w-full flex items-center justify-start">
+         <h1 className="text-3xl font-bold">Job Details</h1>
+      </main>
+   );
+}
